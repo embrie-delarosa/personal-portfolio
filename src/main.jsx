07a7a1fbcf4d3796b1b7b5cde6df7ca3506607e3
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-10-14 16:38:53 +0800

    Moving css and js folder in src folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './assets/styles.css';
+import './css/styles.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
